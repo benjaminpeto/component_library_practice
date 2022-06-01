c1a5f0d675e9c15916ee36709ef0205052ac1be9
Benji <benjaminpeto23@gmail.com>
2022-06-01 11:59:34 +0200

    remove default story component from tsdx, modify index.tsx accordingly, edit README.md

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,3 @@
 //import React, { FC, HTMLAttributes, ReactChild } from 'react';
 export * from './ToggleSwitch';
 export * from './Button';
-
-// export interface Props extends HTMLAttributes<HTMLDivElement> {
-//   /** custom content, defaults to 'the snozzberries taste like snozzberries' */
-//   children?: ReactChild;
-// }
-
-// // Please do not use types off of a default export module or else Storybook Docs will suffer.
-// // see: https://github.com/storybookjs/storybook/issues/9556
-// /**
-//  * A custom Thing component. Neat!
-//  */
-// export const Thing: FC<Props> = ({ children }) => {
-//   return (
-//     <div className="mt-10 bg-yellow-500 p-5 rounded-md">
-//       {children || `the snozzberries taste like snozzberries`}
-//     </div>
-//   );
-// };
